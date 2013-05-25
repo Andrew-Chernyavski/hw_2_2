f34b2628a92153b2426e691495fb09c3705faf67
Andrew-Chernyavski <andrew.chernyavski@yandex.ru>
2013-05-25 11:44:55 +0400

    3.6 and 4.1 are corrected

--- hw3/hw6.txt.hs
+++ hw3/hw6.txt.hs
@@ -1,20 +1,9 @@
--- a ()
--- b {}
--- c []
--- d <>
+check s "" = null s
+check s (c:cs) = if (c `elem` "(<{[")
+    then check (c:s) cs
+    else if (null s || not (((head s):c:[]) `elem` ["()", "<>", "{}", "[]"]))
+            then False
+            else check (tail s) cs
 
 checkLine :: String -> Bool
-checkLine (x:xs) = parse 0 0 0 0 (x:xs)
-		where 
-		parse a b c d [] = ((a == 0) && (b == 0) && (c == 0) && (d == 0))
-		parse a b c d (y:ys) | ((a < 0) || (b < 0) || (c < 0) || (d < 0)) = False
-				      | otherwise = if y == '(' then parse (a + 1) b c d ys
-					       else if y == ')' then parse (a - 1) b c d ys
-					       else if y == '{' then parse a (b + 1) c d ys
-					       else if y == '}' then parse a (b - 1) c d ys
-					       else if y == '[' then parse a b (c + 1) d ys
-					       else if y == ']' then parse a b (c - 1) d ys
-					       else if y == '<' then parse a b c (d + 1) ys
-					       else if y == '>' then parse a b c (d - 1) ys
-                                               else parse a b c d ys
-					      
+checkLine = check "" . filter (`elem` "()<>{}[]")
--- hw4/hw1.hs
+++ hw4/hw1.hs
@@ -1,8 +1,11 @@
-data Tree a = Nil
-            | Branch a (Tree a) (Tree a)
+import Control.Monad
+import Data.List
 
-findByCond :: (a -> Bool) -> (Tree a) -> Bool
-findByCond cond Nil = False
-findByCond cond (Branch v left right) = if cond v 
-					then True
-					else ((findByCond cond left) || (findByCond cond right))
+data BTree a = Null
+                  | Node a (BTree a) (BTree a)
+    deriving (Show)
+
+findByCond :: (a -> Bool) -> BTree a -> Maybe a
+findByCond _ Null = Nothing
+findByCond p (Node x l r) | p x = Just x
+                          | otherwise = findByCond p l `mplus` findByCond p r
